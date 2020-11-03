a70693d1c3fa05cda5ea3b02673a213fffeea722
markblack96 <markblack96@gmail.com>
2020-11-03 11:01:31 -0600

    descriptive stats and data endpoint

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def scripts(filename):
     return static_file(filename, root='./public/')
     # this can't be a great idea but i don't wanna figure out the template stuff rn tbqh
 
+@route('/data/<filename>')
+def return_data(filename):
+    return static_file(filename, root='./data/')
+
 @route('/caddo.json')
 def caddo():
     return static_file('caddo.json', root='./data/')
@@ -18,8 +22,5 @@ def caddo():
 def bossier():
     return static_file('bossier.json', root='./data/')
 
-@route('/caddo-summary.json')
-def caddo_summary():
-    return static_file('caddo-summary.json', root='./data/')
 
 run(host='localhost', port=5000)
--- collect_and_process.py
+++ collect_and_process.py
@@ -18,6 +18,16 @@ caddo_json = caddo_data[['Lab Collection Date', 'Daily Test Count', 'Daily Posit
 
 bossier_json = bossier_data[['Lab Collection Date', 'Daily Test Count', 'Daily Positive Test Count', 'Daily Case Count', 'datetime']].reset_index(drop=True).to_json(orient='records')
 
+
+for data in [caddo_data, bossier_data]:
+    descriptive_stats = {}
+    descriptive_stats['Parish'] = str(data['Parish'].iloc[0])
+    descriptive_stats['Total Tests'] = int(data['Daily Test Count'].sum())
+    descriptive_stats['Total Cases'] = int(data['Daily Case Count'].sum())
+    descriptive_stats['Latest Data'] = str(data['Lab Collection Date'].max().ctime())
+    with open(f"data/{descriptive_stats['Parish'].lower()}Descrip.json", 'w') as f:
+        f.write(json.dumps(descriptive_stats))
+
 # save the data
 with open('data/caddo.json', 'w') as f:
     f.write(caddo_json)
